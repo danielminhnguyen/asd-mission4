60b10a8aba5349b6860cf054c354d83b0ba4796c
DANIEL NGUYEN <hokkinxx@DANIELs-MBP.lan>
2021-02-19 00:56:20 +1300

    add test for functions'

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { checkRego, checkLicence } from "./utils";
 
-test("renders learn react link", () => {
+test("renders main page", () => {
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/Turners Car Insurance/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("check Rego", () => {
+  const rego = "ABC13$";
+  expect(checkRego(rego)).toEqual(false);
+});
+
+test("check Licence", () => {
+  // more than 8 character
+  const licence = "ABC133424";
+  expect(checkLicence(licence)).toEqual(false);
 });
