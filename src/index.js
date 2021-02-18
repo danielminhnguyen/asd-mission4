bb6adf92aa3e06a5ccffe31fad56bbfe69e4c44b
DANIEL NGUYEN <hokkinxx@DANIELs-MacBook-Pro.local>
2021-02-18 17:41:36 +1300

    modify homepage

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={"/mission4"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
