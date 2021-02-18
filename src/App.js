d3985dd91c23d9c18d5b5852c8046365807f4185
DANIEL NGUYEN <hokkinxx@DANIELs-MBP.lan>
2021-02-18 23:28:37 +1300

    modify travis yml

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import Main from "./layout/Main";
 import { defaultTheme } from "./themes";
 import { ThemeProvider } from "@material-ui/styles";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 // import { Route, Switch } from "react-router";
 
 function App() {
   return (
     <div className="App">
-      <ThemeProvider theme={defaultTheme}>
-        <Main />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={defaultTheme}>
+          <Main />
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
